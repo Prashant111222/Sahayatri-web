df59acb836c588504de81321e7cf9d7787efa5e0
Prashant Nepal <prashantnepal567@gmail.com>
2022-06-06 10:45:46 +0545

    Remove redundant folders

--- Saha-Yatri/resources/views/layouts/main/guest.blade.php
+++
@@ -1,9 +0,0 @@
-@include('layouts.navbars.navs.guest')
-<div class="wrapper wrapper-full-page">
-    <div class="page-header login-page header-filter" filter-color="rose"
-        style="background-image: url('{{ asset('material') }}'); background-size: cover; background-position: top center;align-items: center;"
-        data-color="purple">
-        @yield('content')
-        @include('layouts.footers.guest')
-    </div>
-</div>
