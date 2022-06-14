ff8b73166c03ac0aae22e3bb5df241465d824cfc
Prashant Nepal <prashantnepal567@gmail.com>
2022-06-14 12:00:38 +0545

    Admin Details through seeder

--- sahayatri/database/seeders/AdminSeeder.php
+++ sahayatri/database/seeders/AdminSeeder.php
@@ -14,5 +14,12 @@ class AdminSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'phone_no' => '9814321111',
+            'password' => Hash::make('adminadmin'),
+            'type' => 'admin'
+        ]);
     }
 }
